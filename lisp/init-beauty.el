bd564f044606f66cf600066c43c688fd296eb02b
nothingthere <m15982038632@gmail.com>
2017-03-04 22:48:20 +0800

    日常跟新

--- lisp/init-beauty.el
+++ lisp/init-beauty.el
@@ -2,51 +2,38 @@
 ;;; Commentary:
 ;;; Code:
 
-(cl-defmacro my/beauty/delete-top/bottom-blanklines(&key bottom)
-  "删除buffer顶部/底部所有空行。默认为删除顶部空行，如果:BOTTOM为no-nil为删除底部空行.
-
-参考：https://www.emacswiki.org/emacs/DeletingWhitespace#toc4"
-  `(my/with-save-everything+widen
-	,(if bottom `(goto-char (point-max)) `(goto-char (point-min)))
-	(delete-blank-lines)
-	(let ((blank-lines (abs
-						,(if bottom
-							 `(skip-chars-backward "\n\t")
-						   `(skip-chars-forward "\n\t")))))
-	  (when (> blank-lines 0)
-		(delete-char ,(if bottom
-						  `blank-lines
-						`(- blank-lines)))))))
+(defun my/beauty/delete-top-blanklines ()
+  "删除顶部所有空格."
+  (interactive )
+  (my/with-save-everything+widen
+   (goto-char (point-min))
+   (when (my/current-line-empty-p)
+	 (kill-line)
+	 (my/beauty/delete-top-blanklines)
+	 )))
 
 (defun my/beauty/leave-1-empty-line()
   "将buffer中多个相邻的空行只留1个."
   (interactive)
   (my/with-save-everything+widen
    (goto-char (point-min))
-   (let ((in-blank-lines-p (my/current-line-empty-p)))
-	 (while(not (eobp))
+   (let ((previous-line-empty-p (my/current-line-empty-p)))					;当前行是否为空
+	 (while (not (eobp))
 	   (forward-line)
-	   (cond ((my/current-line-empty-p)
-			  (if in-blank-lines-p
-				  (delete-blank-lines)
-				(setq in-blank-lines-p t)))
-			 (t (setq in-blank-lines-p nil)))))))
+	   (cond ((my/current-line-empty-p)	;如果当前行为空
+			  (if previous-line-empty-p	;且上一行也为空
+				  (delete-blank-lines)	;则保留一个空行
+				(setq previous-line-empty-p t))) ;否则标记上一行为空
+			 (t (setq previous-line-empty-p nil)))))))
 
 (defun my/beauty/delete-extra-spaces()
   "删除buffer多余空行，且同时删除行末多余空格。"
   (my/with-save-everything+widen
-   ;; 删除buffer末空行
-   ;; (my/beauty/delete-top/bottom-blanklines :bottom t)
-   ;; 将变量delete-trailing-lines设置为non-nil后，
-   ;; 调用delete-trailing-whitespace命令就可删除buffer末所有空行。
-   ;; 删除所有行末空白
+   ;; 删除行末所有空格
    (setq delete-trailing-lines t)
-
    (delete-trailing-whitespace)
-
-   ;;删除顶部多余空行
-   (my/beauty/delete-top/bottom-blanklines)
-
+   ;;删除顶部所有空格
+   (my/beauty/delete-top-blanklines)
    ;;删除中间的多余空行
    (my/beauty/leave-1-empty-line)
    ))
