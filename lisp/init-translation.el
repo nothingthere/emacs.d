08395e5b26511f8edae0381be3837a6022235fb3
nothingthere <m15982038632@gmail.com>
2017-04-13 14:56:21 +0800

    日常更新:smile:

--- lisp/init-translation.el
+++ lisp/init-translation.el
@@ -3,24 +3,12 @@
 ;; Created: 2017-04-13 14:06:10
 ;;; Commentary:
 ;; 使用Bing的API翻译
-;; 网上抄的，有空自己写一个：https://github.com/coordinate/bingapiel
+;; 有空希望自己写一个
 ;;; Code:
 
-(add-to-list 'load-path "~/.emacs.d/bingapiel")
-
-(defvar bingapi-clientid "667f3adb-e22d-4dca-b476-ca536c0a6d8a")
-;; Your bing api client_secret.
-(defvar bingapi-clientsecret "xBTJ5Ee5RSFf++uVjSVKVFcsoswQlDyb8kPp5wSyrV8=")
-;; list all language codes may be used
-(defvar bingtranslate-language-list '("en" "zh-CHS"))
-;; Turn auto detect language code on
-(defvar bingtranslate-auto-detect t)
-(require 'bing-translate)
-
-(bingtranslate-add-pair "英->中" "en" "zh-CHS")
-(bingtranslate-add-pair "中->英" "zh-CHS" "en")
-
-(bind-key "C-c b" #'bingtranslate-region-or-input)
+(use-package bing-dict
+  :bind
+  ("C-c b" . bing-dict-brief))
 
 (provide 'init-translation)
 ;;; init-translation.el ends here
